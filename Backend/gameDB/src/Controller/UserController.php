4146ae037d50ff8a689eea3c77abdf637dcbb0d6
andreibodo <bodorinandrei@gmail.com>
2021-03-31 13:08:11 +0200

    playlist%

--- Backend/gameDB/src/Controller/UserController.php
+++ Backend/gameDB/src/Controller/UserController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Repository\GameRepository;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,15 +18,15 @@ class UserController extends AbstractController
     /**
      * @Route("/register", name="register", methods={"POST"})
      */
-    public function register(Request $request, EntityManagerInterface $em,UserPasswordEncoderInterface $encoder): Response
+    public function register(Request $request, EntityManagerInterface $em, UserPasswordEncoderInterface $encoder): Response
     {
 
         $userData = json_decode($request->getContent());
 
-        $user=new User;
+        $user = new User;
         $user->setUsername($userData->username);
         $user->setEmail($userData->email);
-        $password=$encoder->encodePassword($user, $userData->password);
+        $password = $encoder->encodePassword($user, $userData->password);
         $user->setPassword($password);
 
         $em->persist($user);
@@ -32,6 +34,74 @@ class UserController extends AbstractController
 
 
 
-       return new JsonResponse($user);
+        return new JsonResponse($user);
+    }
+
+    /**
+     * @Route("/playlist/{username}", name="playlist", methods={"GET"})
+     */
+    public function playlist(UserRepository $repo, $username): Response
+    {
+        $userDataRaw = $repo->findByUsername($username);
+
+        $playlist = ["playlist" => []];
+
+        foreach ($userDataRaw as $userData) {
+            foreach ($userData->getPlaylist() as $playlistElement) {
+
+                $game = [
+                    "id" => $playlistElement->getId(),
+                    "name" => $playlistElement->getName(),
+                    "description" => $playlistElement->getDescription(),
+                    "poster" => $playlistElement->getPoster(),
+                    "images" => $playlistElement->getImages(),
+                    "clip" => $playlistElement->getClip(),
+                    "releaseDate" => $playlistElement->getReleaseDate(),
+                    "platforms" => $playlistElement->getPlatforms(),
+                    "pcMinimum" => $playlistElement->getPcMinimum(),
+                    "pcRecomended" => $playlistElement->getPcRecomended(),
+                    "genres" => $playlistElement->getGenres(),
+                    "developer" => $playlistElement->getDeveloper(),
+                    "metacritic" => $playlistElement->getMetacriticRating(),
+                    "dlcs" => []
+                ];
+
+                foreach ($playlistElement->getDlcs() as $dlcData) {
+                    $dlc = [
+                        "name" => $dlcData->getName(),
+                        "description" => $dlcData->getDescription(),
+                        "poster" => $dlcData->getPoster(),
+                        "releaseDate" => $dlcData->getReleaseDate(),
+                    ];
+
+                    $game["dlcs"][] = $dlc;
+                }
+
+                $playlist["playlist"][]=$game;
+            }
+        }
+
+
+        return new JsonResponse($playlist);
+    }
+
+    /**
+     * @Route("/playlist/add/{username}/{gameId}", name="add", methods={"GET"})
+     */
+    public function addToPlaylist(EntityManagerInterface $em,UserRepository $repo,GameRepository $gameRepo, $username,$gameId): Response
+    {
+        $userRaw = $repo->findByUsername($username);
+        $gameRaw= $gameRepo->findById($gameId);
+
+        foreach($userRaw as $user){
+            foreach($gameRaw as $game){
+                $user->addPlaylist($game);
+
+                $em->persist($user);
+                $em->flush();
+            }
+        }
+        
+        return new JsonResponse(["ok"]);
     }
 }
